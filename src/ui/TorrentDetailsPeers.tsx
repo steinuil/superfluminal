db22328a0f27cfbb0e4a13a4fff7b835f76ac67a
steinuil <steenuil.owl@gmail.com>
2020-04-22 14:39:37 +0200

    factored Peer into component

--- src/ui/TorrentDetailsPeers.tsx
+++ src/ui/TorrentDetailsPeers.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useMemo, FC } from 'react';
 import { createUseStyles } from 'react-jss';
 import { PeerResource } from '../types/SynapseProtocol';
 import { fmtProgress, fmtBitrateBin } from '../Units';
 import { FiArrowUp, FiArrowDown } from 'react-icons/fi';
 import { TextSingleLine } from '../components/TextSingleLine';
+import { countryCodeToEmoji } from '../GetCountryByIp';
 
 const useStyles = createUseStyles({
   table: {
@@ -25,6 +26,34 @@ const useStyles = createUseStyles({
   },
 });
 
+interface PeerProps {
+  ip: string;
+  availability: number;
+  rateUp: number;
+  rateDown: number;
+}
+
+const Peer: FC<PeerProps> = ({ ip, availability, rateUp, rateDown }) => {
+  const styles = useStyles();
+
+  return (
+    <tr className={styles.row}>
+      <td className={styles.cell}>{ip.split(':')[0]}</td>
+      <td className={styles.cell}>{fmtProgress(availability)}</td>
+      <td className={styles.cell}>
+        <div className={styles.rate}>
+          <FiArrowUp className={styles.arrowIcon} /> {fmtBitrateBin(rateUp)}
+        </div>
+      </td>
+      <td className={styles.cell}>
+        <div className={styles.rate}>
+          <FiArrowDown className={styles.arrowIcon} /> {fmtBitrateBin(rateDown)}
+        </div>
+      </td>
+    </tr>
+  );
+};
+
 interface Props {
   peers: PeerResource[];
 }
@@ -36,22 +65,13 @@ export const TorrentDetailsPeers: React.FC<Props> = ({ peers }) => {
     <table className={styles.table}>
       <tbody>
         {peers.map(({ id, ip, availability, rate_up, rate_down }) => (
-          <tr key={id} className={styles.row}>
-            <td className={styles.cell}>{ip}</td>
-            <td className={styles.cell}>{fmtProgress(availability)}</td>
-            <td className={styles.cell}>
-              <div className={styles.rate}>
-                <FiArrowUp className={styles.arrowIcon} />{' '}
-                {fmtBitrateBin(rate_up)}
-              </div>
-            </td>
-            <td className={styles.cell}>
-              <div className={styles.rate}>
-                <FiArrowDown className={styles.arrowIcon} />{' '}
-                {fmtBitrateBin(rate_down)}
-              </div>
-            </td>
-          </tr>
+          <Peer
+            key={id}
+            ip={ip}
+            availability={availability}
+            rateUp={rate_up}
+            rateDown={rate_down}
+          />
         ))}
       </tbody>
     </table>
