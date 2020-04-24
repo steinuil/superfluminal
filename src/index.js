6eabc33a50ba53047df778c20e1b2f069926510f
steinuil <steenuil.owl@gmail.com>
2020-04-24 15:07:56 +0200

    cleanup 2

--- src/index.js
+++ src/index.js
@@ -2,18 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
-import query from 'query-string';
 
-// import store, { create, history } from './storeOld';
 import { history, store } from './Store';
-// import '../scss/main.scss';
-// import 'bootstrap/scss/bootstrap.scss';
-import { ws_init } from './socket';
-import { filter_subscribe } from './actions/filter_subscribe';
-import { socket_uri, socket_update, SOCKET_STATE } from './actions/socket';
-import { search_criteria } from './searchOld';
 
-import Connection from './ui/connection';
 import { Layout } from './ui/Layout';
 
 Set.prototype.difference = function (set) {
@@ -23,25 +14,6 @@ Set.prototype.difference = function (set) {
   }
   return diff;
 };
-
-export function initialize(uri) {
-  store.dispatch(socket_uri(uri));
-  store.dispatch(socket_update(SOCKET_STATE.CONNECTING));
-  ws_init(
-    uri,
-    () => {
-      const qs = query.parse(window.location.search);
-      store.dispatch(socket_update(SOCKET_STATE.CONNECTED));
-      store.dispatch(filter_subscribe('torrent', search_criteria(qs.s)));
-      store.dispatch(filter_subscribe('server'));
-    },
-    () => {
-      store.dispatch(
-        socket_update(SOCKET_STATE.DISCONNECTED, 'You were disconnected.')
-      );
-    }
-  );
-}
 
 ReactDOM.render(
   <Provider store={store}>
